a99c910a915a4ebfd799d02350eae1fe9f6551ed
Alfonsosh02 <alfonsohenao08@gmail.com>
2024-02-26 14:11:39 +0100

    WichTime acabado

--- Assets/Scripts/WitchTime.cs
+++ Assets/Scripts/WitchTime.cs
@@ -7,21 +7,22 @@ public class WitchTime : MonoBehaviour
     private float normalTimeScale = 1.0f;
     private float slowedTimeScale = 0.25f;
     private float startTime;
-    private float witchTimeDuration = 0f; 
+    private float witchTimeDuration = 0f;
+    public AudioClip witchTimeAudioClip;
 
     void Start()
     {
-        StartCoroutine(ActivateWitchTime());
+        
     }
 
     IEnumerator ActivateWitchTime()
     {
-        AudioClip witchTimeAudioClip = Resources.Load<AudioClip>("witchTime"); 
-        AudioManager.instance.PlayAudio(witchTimeAudioClip, "WitchTimeAudio", false, 1.0f);
+    
+        AudioSource source = AudioManager.instance.PlayAudio(witchTimeAudioClip, "WitchTimeAudio", false, 1.0f);
         witchTimeDuration = witchTimeAudioClip.length;
         Time.timeScale = slowedTimeScale;
         startTime = Time.time;
-        while (Time.time - startTime < witchTimeDuration)
+        while (source && source.isPlaying)
         {
             yield return null;
         }
@@ -33,12 +34,9 @@ public class WitchTime : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(1))
         {
-          
+            StartCoroutine(ActivateWitchTime());
             Time.timeScale = slowedTimeScale;
         }
-        else if (Input.GetMouseButtonUp(1))
-        {
-            Time.timeScale = normalTimeScale;
-        }
+       
     }
 }
