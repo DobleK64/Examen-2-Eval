613426eb0ab10e11bb08eb6943f9aa9daf025189
Alfonsosh02 <alfonsohenao08@gmail.com>
2024-02-26 13:47:56 +0100

    WitchTime casi acabado

--- Assets/Scripts/WitchTime.cs
+++ Assets/Scripts/WitchTime.cs
@@ -4,9 +4,41 @@ using UnityEngine;
 
 public class WitchTime : MonoBehaviour
 {
-    // Update is called once per frame
+    private float normalTimeScale = 1.0f;
+    private float slowedTimeScale = 0.25f;
+    private float startTime;
+    private float witchTimeDuration = 0f; 
+
+    void Start()
+    {
+        StartCoroutine(ActivateWitchTime());
+    }
+
+    IEnumerator ActivateWitchTime()
+    {
+        AudioClip witchTimeAudioClip = Resources.Load<AudioClip>("witchTime"); 
+        AudioManager.instance.PlayAudio(witchTimeAudioClip, "WitchTimeAudio", false, 1.0f);
+        witchTimeDuration = witchTimeAudioClip.length;
+        Time.timeScale = slowedTimeScale;
+        startTime = Time.time;
+        while (Time.time - startTime < witchTimeDuration)
+        {
+            yield return null;
+        }
+        Time.timeScale = normalTimeScale;
+    }
+
+
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(1))
+        {
+          
+            Time.timeScale = slowedTimeScale;
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            Time.timeScale = normalTimeScale;
+        }
     }
 }
